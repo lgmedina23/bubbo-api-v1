e87b4e665c310fb8bdeff2cf394d5109b8d469b2
Luis Guillermo <lgmedina23@lgmedina23.local>
2024-04-10 16:03:12 +0200

    module and db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { Todo } from './todos/entities/todo.entity';
+
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(),TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    entities: [
+   Todo
+    ],
+
+    synchronize: true,
+    migrations: [],
+    subscribers: [],
+  }),TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/todos/entities/todo.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Todo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ default: false })
+  completed: boolean;
+
+  @Column({ nullable: true })
+  description?: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
